39d2b041afaee355270901a3a46abac527c8dc18
Spencer K. Monckton <46308647+skmonckton@users.noreply.github.com>
2026-03-28 21:58:50 -0400

    Attempted fix to build action

--- src/install-R.js
+++ src/install-R.js
@@ -1,8 +1,10 @@
 // scripts/install-R.js
 import { execSync } from "child_process";
+import { pathToFileURL } from "url";
 
 const nhyrisRoot = execSync("npm root -g").toString().trim();
-const { installStandaloneR, installDependencies } = await import(`${nhyrisRoot}/nhyris/utils/install.js`);
+const nhyrisInstall = pathToFileURL(`${nhyrisRoot}/nhyris/utils/install.js`).href;
+const { installStandaloneR, installDependencies } = await import(nhyrisInstall);
 
 installStandaloneR(process.cwd());
 installDependencies(process.cwd());
